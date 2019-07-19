5871d32b78816bd4c89c1d6efc8b8178678a6b29
Krzysztof Narloch <knarloch+GITHUB@gmail.com>
2019-07-19 10:50:38 +0200

    Added tagDispatch.cpp
    Based on example in the book "Effecive Modern C++" Item 26.

---
+++ tagDispatch.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <type_traits>
+
+template<typename T>
+void eatFloatingPointImpl(T&& meal, std::false_type)
+{
+	std::cout<<"bleh! "<<meal<<"\n";
+}
+
+template<typename T>
+void eatFloatingPointImpl(T&& meal, std::true_type)
+{
+	std::cout<<"nom nom nom "<<meal<<"\n";
+}
+
+template<typename T>
+void eatFloatingPoint(T&& meal)
+{
+	//eatFloatingPointImpl(meal, std::is_floating_point<std::remove_reference_t<T>>()); //cpp14
+	eatFloatingPointImpl(meal, std::is_floating_point<typename std::remove_reference<T>::type>()); //cpp11
+}
+
+int main()
+{
+	eatFloatingPoint(1);
+	eatFloatingPoint(1.5);
+	double f = 2.5;
+	eatFloatingPoint(f);
+	eatFloatingPoint(std::string{"string"});
+	return 0;
+}
+	
+	
