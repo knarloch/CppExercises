b2741c2f7200621605ceec8df323447e58e88b49
Krzysztof Narloch <knarloch+GITHUB@gmail.com>
2025-03-05 23:01:24 +0100

    Rework cake_cutting excersise
    Modify examples to match prompt.
    Provide a solution using line sweep algorithm.

--- cake_cutting/README.md
+++ cake_cutting/README.md
@@ -2,11 +2,11 @@
 
 ## Cake cutting
 
-On a rectangle table there are pieces of cake. Cut the table alongside (give Y coordinate) so that the ammount of cake
+On a rectangle table there are pieces of cake. Cut the table alongside (give Y coordinate) so that the amount of cake
 in both parts of the table is the same.
 
 Input: `[Cake(x=1.0, y=0.0, side=2.0), Cake(x=4.0, y=1.0, side=2.0)]`  
-Output: between 3.0 and 4.0
+Output: 1.5
 
 ```
 0   1   2   3   4   5   6
--- cake_cutting/cake_cutting.cpp
+++ cake_cutting/cake_cutting.cpp
@@ -1,6 +1,5 @@
+#include <algorithm>
 #include <vector>
-#include <set>
-#include <tuple>
 #include <cassert>
 
 using namespace std;
@@ -13,103 +12,66 @@ struct Cake {
     double side{};
 };
 
-struct Interval {
-    explicit Interval(const Cake &cake) {
-        x_min = cake.x;
-        x_max = cake.x + cake.side;
-        a = cake.side;
-    }
-
-    Interval(double x_min_, double x_max_, double a_) : x_min{x_min_}, x_max{x_max_}, a{a_} {}
-
-    double x_min{};
-    double x_max{};
+struct IntervalBorder {
+    double y0{};
     double a{};
+    double offset{};
 
+    double cake_ammount(double y) const {
+        return a * (y - y0) + offset;
+    }
 };
 
-bool operator<(const Interval &l, const Interval &r) {
-    if (l.x_min == r.x_min) {
-        return l.x_max < r.x_max;
-    }
-    return l.x_min < r.x_min;
+bool operator<(const IntervalBorder &l, const IntervalBorder &r) {
+    return l.y0 < r.y0;
 }
 
-std::tuple<Interval, Interval, Interval> OverlapIntervals(const Interval &l, const Interval &r) {
-    Interval leftmost(l.x_min, r.x_min, l.a);
-    Interval middle(r.x_min, std::min(l.x_max, r.x_max), l.a + r.a);
-    Interval rightmost(std::min(l.x_max, r.x_max), std::max(l.x_max, r.x_max), l.x_max < r.x_max ? r.a : l.a);
-    return {leftmost, middle, rightmost};
-}
-
-double TotalCake(const std::vector<Cake> &data) {
-    double result = 0;
-    for (const auto &cake: data) {
-        result += cake.side * cake.side;
+double FindCuttingY(const std::vector<Cake> cakes) {
+    double target = 0.0;
+    std::vector<IntervalBorder> ibs;
+    ibs.reserve(cakes.size() * 2);
+    for (const auto &cake: cakes) {
+        ibs.push_back(IntervalBorder{.y0 = cake.y, .a = cake.side});
+        ibs.push_back(IntervalBorder{.y0 = cake.y + cake.side, .a = -1.0 * cake.side});
+        target += cake.side * cake.side * 0.5;
     }
-    return result;
-}
+    std::sort(ibs.begin(), ibs.end());
 
-
-double FindCuttingPoint(const std::vector<Cake> &data) {
-    double target = TotalCake(data) / 2;
-    std::set<Interval> intervals;
-
-    for (const auto &cake: data) {
-        if (cake.side == 0) {
-            continue;
+    // do a rolling sum of interval borders
+    // in other words treat ibs as derivatives
+    // and integrate them back to cake_amount(y) function.
+    for (auto i = 1u; i < ibs.size(); i++) {
+        ibs[i].a += ibs[i - 1].a;
+        ibs[i].offset = ibs[i - 1].cake_ammount(ibs[i].y0);
+        if (ibs[i].cake_ammount(ibs[i].y0) >= target) {
+            const auto &target_interval = ibs[i - 1];
+            // target == f(y) == a(y-y0) + offset;
+            return (target - target_interval.offset) / target_interval.a + target_interval.y0;
         }
-        Interval dummy{cake};
-        auto it = intervals.lower_bound(dummy);
-        while (it != intervals.end() and dummy.x_min < it->x_max) {
-            auto extracted = *it;
-            auto [l, m, r] = OverlapIntervals(extracted, dummy);
-            intervals.erase(it);
-            if(l.x_max-l.x_min>0){
-                intervals.insert(l);
-            }
-            if(m.x_max-m.x_min>0){
-                intervals.insert(m);
-            }
-
-            auto [inserted_it, _] = intervals.insert(m);
-            dummy = r;
-            it = inserted_it++;
-        }
-        if(dummy.x_max-dummy.x_min>0){
-            intervals.insert(dummy);
-        }
-    }
-
-    double cake_so_far = 0;
-    for (const auto &interval: intervals) {
-        if (target <= cake_so_far + interval.a * (interval.x_max - interval.x_min)) {
-            return (target - cake_so_far) / interval.a + interval.x_min;
-        }
-        cake_so_far += +interval.a * (interval.x_max - interval.x_min);
     }
     return -1.0;
 }
 
 int main() {
     {
-        std::vector<Cake> data = {Cake(1.0, 1.0, 2.0), Cake(4.0, 1.0, 2.0)};
-        assert(3.0 == FindCuttingPoint(data));
+        std::vector<Cake> data = {Cake(1.0, 0.0, 2.0), Cake(4.0, 1.0, 2.0)};
+        assert(1.5 == FindCuttingY(data));
     }
     {
-        std::vector<Cake> data = {Cake(0.0, 1.0, 10.0)};
-        assert(5.0 == FindCuttingPoint(data));
+        std::vector<Cake> data = {Cake(1.0, 0.0, 10.0)};
+        assert(5.0 == FindCuttingY(data));
     }
     {
-        std::vector<Cake> data = {Cake(0.0, 1.0, 10.0), Cake(0.0, 1.0, 1.0), Cake(12, 2.0, 1.0)};
-        assert(5.0 == FindCuttingPoint(data));
+        std::vector<Cake> data = {Cake(1.0, 1.0, 10.0)};
+        assert(6.0 == FindCuttingY(data));
     }
     {
-        std::vector<Cake> data = {Cake(0.0, 1.0, 4), Cake(3.0, 1.0, 1.0)};
-        assert(2.125 == FindCuttingPoint(data));
+        std::vector<Cake> data = {Cake(1.0, 0.0, 10.0), Cake(21.0, 0.0, 1.0), Cake(22.0, 9.0, 1.0)};
+        assert(5.0 == FindCuttingY(data));
     }
-
-
-
+    {
+        std::vector<Cake> data = {Cake(1.0, 0.0, 10.0), Cake(21.0, 0.0, 1.0), Cake(22.0, 19.0, 1.0)};
+        assert(5.0 == FindCuttingY(data));
+    }
     return 0;
 }
