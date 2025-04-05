4b6e797c1b631c634ecb8dd95509c979a6a0b372
Krzysztof Narloch <knarloch+GITHUB@gmail.com>
2025-04-05 09:25:46 +0200

    Add Search in rotated sorted array excersise.

---
+++ search_in_rotated_array/search_in_rotated_array.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <cassert>
+#include <vector>
+
+int search_in_rotated_array(int target, const std::vector<int>& array){
+    if(array.empty()){
+        return -1;
+    }
+
+    auto pp = std::partition_point(array.begin(), array.end(), [first_partition_min = array[0]](int value){
+        return value<first_partition_min;
+    });
+
+    auto candidate1 = std::find(array.begin(), pp, target);
+    if(candidate1 < pp){
+        return candidate1-array.begin();
+    }
+    auto candidate2 = std::find(pp, array.end(), target);
+    if(candidate2 < array.end()){
+        return candidate2-array.begin();
+    }
+    return -1;
+}
+
+
+
+int main(){
+    {
+        assert(-1 == search_in_rotated_array(2345, {}));
+    }
+    {
+        assert(0 == search_in_rotated_array(1, {1}));
+        assert(0 == search_in_rotated_array(1, {1, 2}));
+        assert(1 == search_in_rotated_array(1, {2, 1}));
+    }
+    {
+        std::vector<int> rotated{4,5,6,7,8,9,1,2,3};
+        for(auto i =0; i<rotated.size(); i++){
+            auto result = search_in_rotated_array(rotated[i], rotated);
+            assert(i == result);
+        }
+        assert(-1 == search_in_rotated_array(2345, rotated));
+
+    }
+    return 0;
+}
